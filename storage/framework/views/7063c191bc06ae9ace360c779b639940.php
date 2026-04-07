4b3d97ba61655ebc70f09411522d464a2959e4a4
Rifkiizzulhaq <Sekilasgadget01@gmail.com>
2026-04-07 18:42:05 +0700

    Ignore and remove generated runtime artifacts

--- storage/framework/views/7063c191bc06ae9ace360c779b639940.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <h1>Halaman Utama</h1>
-<?php $__env->stopSection(); ?>
-
-
-<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Rifki Izzulhaq\Documents\Skripsi\Project\Web\laravel\example-app\resources\views/User/user-halaman-utama/UserHalamanUtama.blade.php ENDPATH**/ ?>
--- storage/framework/views/e4e6af2eff3734f48ff4d5c94df7c904.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <h1>Admin Dashboard</h1>
-<?php $__env->stopSection(); ?>
-
-
-
-<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Rifki Izzulhaq\Documents\Skripsi\Project\Web\laravel\example-app\resources\views/Admin/admin-halaman-utama/AdminHalamanUtama.blade.php ENDPATH**/ ?>
